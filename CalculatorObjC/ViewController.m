30d0c69679c9ddad259e79d6bfef513af260d630
TebogoSibiya <kapiten.bm@gmail.com>
2024-01-16 15:29:27 +0200

    Using the _Current variable instead of _Expression

--- CalculatorObjC/ViewController.m
+++ CalculatorObjC/ViewController.m
@@ -28,50 +28,61 @@ NSMutableString *current=@"";
 }
 
 - (IBAction)tapNumber:(UIButton *)sender {
+    if([_lblExpression.text isEqualToString:@"Expression"]){[_lblExpression setText:@""];}
     if([current isEqualToString:@"0"]) {
         [current setString: sender.titleLabel.text];
-        [_expression setString:[[_expression substringToIndex:lastSymbolIndex-1] stringByAppendingString:current]];
+//        [_expression setString:[[_expression substringToIndex:lastSymbolIndex-1] stringByAppendingString:current]];
     }
 //    else if(_expression.length>1&&[[_expression substringFromIndex:_expression.length-1] isEqualToString:@"0"]&&![NSString stringWithCharacters:[_expression characterAtIndex:_expression.length-2] length:1].floatValue){
 //        [_lblExpression setText:[_expression substringToIndex:_expression.length-2]];}
-    else {[_expression appendString: sender.titleLabel.text];}
-    [_lblExpression setText:_expression];
+    else {[current appendString: sender.titleLabel.text];}
+    [self appendExpression:@""];
+//    [_lblExpression setText:[[_lblExpression.text substringToIndex:lastSymbolIndex] stringByAppendingString:current]];
     
 //    if(_expression.length>1){NSLog(@"%@:%@",  @"Expression#charAt-2", [_expression characterAtIndex:_expression.length-2]);}
 }
 
 - (IBAction)tapSymbol:(UIButton *)sender {
-    if(![_expression substringToIndex:_expression.length-1].intValue&&lastSymbolIndex==_expression.length-1){return;}
-    if(_expression.length<1){if(sender.tag<3){[_expression setString:@"0"];}else{[_expression setString:@"1"];}}
+    if(_lblExpression.text.length>1&&lastSymbolIndex==_lblExpression.text.length-1){return;}
+    if(current.length<1){if(sender.tag<3){[current setString:@"0"];}else{[current setString:@"1"];}}
     
 //    NSObject obj =[_lblExpression.text substringFromIndex:_lblExpression.text.length-1]
-    if ([_expression substringFromIndex:_expression.length-1].floatValue||[_expression isEqualToString:@"0"]) {
+    if (current.floatValue||[current isEqualToString:@"0"]) {
         switch (sender.tag) {
-            case 1: [_expression appendString: @"+"]; break;
-            case 2: [_expression appendString: @"-"]; break;
-            case 3: [_expression appendString: @"÷"]; break;
-            case 4: [_expression appendString: @"×"]; break;
-            case -1: [_expression appendString: sender.titleLabel.text]; break;
+            case 1: [self appendExpression: @"+"]; break;
+            case 2: [self appendExpression: @"-"]; break;
+            case 3: [self appendExpression: @"÷"]; break;
+            case 4: [self appendExpression: @"×"]; break;
+            case -1: [self appendExpression: sender.titleLabel.text]; break;
             default: break;
         }
+        lastSymbolIndex=_lblExpression.text.length-1;
+        [current setString:@""];
     }
     
-    [_lblExpression setText:_expression];
-    lastSymbolIndex=_expression.length-1;
-    [current setString:@""];
+//    [_lblExpression setText:_expression];
 }
 
 - (IBAction)tapClear:(UIButton *)sender {[self clearAll];}
 
 - (IBAction)tapBackspace:(UIButton *)sender {
-    if(_expression.length>0){[_expression setString:[_expression substringToIndex:_expression.length-1]];}
-    if(_expression.length<=0){[self clearAll]; return;}
+    if(current.length>0){[current setString:[current substringToIndex:current.length-1]];}
+    if(current.length<=0){[self clearAll]; return;}
     [_lblExpression setText:_expression];
 }
 
 - (void)clearAll {
     [_lblExpression setText:@"Expression"];
-    [_expression setString:@""];
+    [current setString:@""];
+}
+
+- (void) appendExpression:(NSString *)val {
+    NSString *toEnter;
+    NSInteger indx=lastSymbolIndex;
+    if([val isEqualToString:@""]){toEnter=current;}
+    else {toEnter=val;}
+    if(indx<1){indx=_lblExpression.text.length-1;}
+    [_lblExpression setText:[[_lblExpression.text substringToIndex:indx] stringByAppendingString:toEnter]];
 }
 
 @end
